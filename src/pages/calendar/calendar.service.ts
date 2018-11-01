1637e1ae7030ebd19fda7a6715b9630764ab036f
aldiazve <aldiazve@gmail.com>
2018-11-01 10:33:03 -0500

    backend ip updated

--- src/pages/calendar/calendar.service.ts
+++ src/pages/calendar/calendar.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 
 export class CalendarService {
-  public mainDomain = 'http://127.0.0.1:10010/';
+  public mainDomain = 'http://192.168.88.57:10010/';
   public BACKEND_ENDPOINTS = {
   	MONTHLY_EVENTS: 'eventos/mes',
   };
