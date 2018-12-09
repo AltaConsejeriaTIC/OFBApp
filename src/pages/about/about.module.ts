5356d315931d507c35c9c8dc01a4d8c5ff9a16bd
Steven Pardo <dspardo6@gmail.com>
2018-12-08 20:36:50 -0500

    Fixed back-button

--- src/pages/about/about.module.ts
+++ src/pages/about/about.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AboutPage } from './about';
 import { ComponentsModule } from '../../components/components.module';
+import { DirectivesModule } from '../../directives/directives.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { ComponentsModule } from '../../components/components.module';
   ],
   imports: [
     IonicPageModule.forChild(AboutPage),
-    ComponentsModule
+    ComponentsModule,
+    DirectivesModule
   ]
 })
 export class AboutPageModule {}
--- src/pages/terms/terms.module.ts
+++ src/pages/terms/terms.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TermsPage } from './terms';
 import { ComponentsModule } from '../../components/components.module';
+import { DirectivesModule } from '../../directives/directives.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { ComponentsModule } from '../../components/components.module';
   ],
   imports: [
     IonicPageModule.forChild(TermsPage),
-    ComponentsModule
+    ComponentsModule,
+    DirectivesModule
   ]
 })
 export class TermsPageModule { }
