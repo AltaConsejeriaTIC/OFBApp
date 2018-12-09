fcae8af495f2eaf4239adeda056fdc44ec7a3952
Steven Pardo <dspardo6@gmail.com>
2018-12-08 20:42:34 -0500

    White background

--- src/pages/event-details/event-details.module.ts
+++ src/pages/event-details/event-details.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { EventDetailsPage } from './event-details';
+import { DirectivesModule } from '../../directives/directives.module';
 
 @NgModule({
   declarations: [
-    EventDetailsPage,
+    EventDetailsPage
   ],
   imports: [
     IonicPageModule.forChild(EventDetailsPage),
-  ],
+    DirectivesModule
+  ]
 })
-export class EventDetailsPageModule {}
+export class EventDetailsPageModule { }
