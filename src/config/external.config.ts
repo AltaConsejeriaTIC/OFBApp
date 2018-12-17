2d098bb36406c91208232ca6f783eb4b505c495f
Steven Pardo <dspardo6@gmail.com>
2018-12-17 12:02:15 -0500

    Aula virtual

--- src/config/external.config.ts
+++ src/config/external.config.ts
@@ -4,5 +4,5 @@ export class ExternalConfig {
   public static spotify = 'https://open.spotify.com/album/0z4qrWLVFyhdXEmKJ9WOQw';
   public static musicalAppreciation = 'http://apreciacionmusicalofb.gov.co/aula/page/2/';
   public static educationalProject = 'http://proyectoeducativo.ofb.gov.co/proyecto-educativo-orquesta-filarmonica-de-bogota/';
-  public static virtualClassroom = 'http://proyectoeducativo.ofb.gov.co/proyecto-educativo-orquesta-filarmonica-de-bogota/';
+  public static virtualClassroom = 'https://sites.google.com/a/ofb.gov.co/ofb-fomento/programas-de-formacion';
 }
