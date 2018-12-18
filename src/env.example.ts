40daaf2f31fea403b6c37d24632821d4fdea0556
Steven Pardo <dspardo6@gmail.com>
2018-12-18 13:46:38 -0500

    Example env

---
+++ src/env.example.ts
@@ -0,0 +1,8 @@
+import { Environment } from './interfaces/environment.interface';
+
+export const ENV: Environment = {
+  endpoint: {
+    app: 'http://165.227.254.52:10010',
+    admin: 'http://165.227.254.52:8080'
+  }
+};
