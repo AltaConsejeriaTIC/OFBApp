171fab70cc6a51df8961011be9a56c3f6c82506c
Steven Pardo <dspardo6@gmail.com>
2019-02-08 00:00:24 -0500

    support <wbr /> tag

--- src/pipes/strip-html/strip-html.ts
+++ src/pipes/strip-html/strip-html.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class StripHtmlPipe implements PipeTransform {
   public transform(value: string, ...without: string[]) {
     return value.replace(/(<([^>]+)>)/ig, (tag) => {
-      const tagName = tag.match(/(<\/?)(\w+)(\s+\w+.*?)?>/i)[2];
-      return without.includes(tagName) ? tag : '';
+      const tagName = tag.match(/(<\/?)(\w+\s?)(\s+\w+.*?)?(\/?>)/i);
+      return tagName && without.includes(tagName[2]) ? tag : '';
     });
   }
 }
